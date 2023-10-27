1792d6443f9fb1a5893b7c6d28f1ccf8bfee9493
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-10-27 20:35:35 +0600

    add: shortvideo component

---
+++ components/Shortvideo.vue
@@ -0,0 +1,310 @@
+<script setup lang="ts">
+//
+</script>
+<template>
+  <section
+    class="w-full flex flex-col gap-x-8 justify-center items-center rounded-3xl bg-[#333] gap-y-4 xl:flex-row xl:p-4 2xl:p-8"
+  >
+    <div class="flex flex-col flex-1">
+      <div
+        id="shortvideo"
+        class="relative flex justify-center items-center bg-black rounded-2xl relative"
+      >
+        <video
+          autoplay
+          class="max-w-screen max-h-screen w-full h-auto block rounded-xl"
+        >
+          <!-- <source
+            src="../assets/video.mp4"
+            type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+          /> -->
+          <source
+            src="../assets/video.webm"
+            type='video/webm; codecs="vp8, vorbis"'
+          />
+          Your browser does not support the video tag.
+        </video>
+
+        <!-- <img
+          src="https://cdn.sanity.io/images/hdokr93d/production/1c905d8fe810e2fe727f41c65416b6ebd15744e2-1920x1080.jpg?w=1920&h=1080"
+          alt=""
+          class="block rounded-xl"
+        /> -->
+        <div id="inner-details" class="w-full h-full absolute">
+          <div
+            class="absolute right-0 bottom-50% translate-y-1/2 w-fit flex flex-col gap-y-4 mr-20px"
+          >
+            <div class="action-btn-mobile">
+              <IconLightning />
+            </div>
+            <div class="action-btn-mobile">
+              <IconChat />
+            </div>
+            <div class="action-btn-mobile">
+              <IconBookmark />
+            </div>
+            <div class="action-btn-mobile">
+              <IconShare />
+            </div>
+            <div class="action-btn-mobile">
+              <IconDots />
+            </div>
+          </div>
+          <div
+            class="flex absolute left-0 bottom-0 flex-col gap-y-2 mt-4 px-8 pl-8 pr-8 pb-8 pt-4"
+          >
+            <div class="flex flex-row justify-between items-center">
+              <div class="flex flex-row items-center gap-x-4">
+                <h3 class="font-main-bold text-3xl">It's Hyperlights</h3>
+              </div>
+            </div>
+            <div class="flex flex-row gap-x-2 items-center">
+              <div class="maintag-set">
+                <img
+                  src="https://img.championat.com/news/big/p/y/v-dota-2-poyavilsya-pervyj-chestnyj-igrok-s-grandmasterom-na-marci-viktoriya-bonya_1637747390104987094.jpg"
+                  alt=""
+                />
+                <h6 class="font-secondary-bold text-lg">mixturegg</h6>
+              </div>
+              <div class="maintag-set">
+                <img
+                  src="https://st5.depositphotos.com/69187398/66863/v/450/depositphotos_668639280-stock-illustration-counter-strike-shooting-game-go1.jpg"
+                  alt=""
+                />
+                <h6 class="font-secondary-bold text-lg">CS:GO</h6>
+              </div>
+              <div class="flex flex-row items-center gap-x-2 ml-4">
+                <div class="hashtag-set">
+                  <IconHash
+                    class="w-[10px] min-w-[10px] h-[10px] min-h-[10px]"
+                  />
+                  <p class="font-secondary-bold">Ace</p>
+                </div>
+                <div class="hashtag-set">
+                  <IconHash
+                    class="w-[10px] min-w-[10px] h-[10px] min-h-[10px]"
+                  />
+                  <p class="font-secondary-bold">Ak47</p>
+                </div>
+                <div class="hashtag-set">
+                  <IconHash
+                    class="w-[10px] min-w-[10px] h-[10px] min-h-[10px]"
+                  />
+                  <p class="font-secondary-bold">Dust2</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="outer-details" class="hidden flex-col gap-y-2 mt-4 p-4 pb-6">
+        <div class="flex flex-row justify-between items-center">
+          <div class="flex flex-row items-center gap-x-4">
+            <h3 class="font-main-bold text-3xl">It's Hyperlights</h3>
+          </div>
+          <div class="flex flex-row items-center gap-x-2">
+            <div class="action-btn">
+              <IconLightning />
+            </div>
+            <div class="action-btn">
+              <IconBookmark />
+            </div>
+            <div class="action-btn">
+              <IconShare />
+            </div>
+            <div class="action-btn">
+              <IconDots />
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex flex-wrap flex-row gap-x-4 gap-y-2 justify-between items-center"
+        >
+          <div class="flex flex-row gap-x-2 items-center mr-4">
+            <div class="maintag-set">
+              <img
+                src="https://img.championat.com/news/big/p/y/v-dota-2-poyavilsya-pervyj-chestnyj-igrok-s-grandmasterom-na-marci-viktoriya-bonya_1637747390104987094.jpg"
+                alt=""
+              />
+              <h6 class="font-secondary-bold text-lg">mixturegg</h6>
+            </div>
+            <div class="maintag-set">
+              <img
+                src="https://st5.depositphotos.com/69187398/66863/v/450/depositphotos_668639280-stock-illustration-counter-strike-shooting-game-go1.jpg"
+                alt=""
+              />
+              <h6 class="font-secondary-bold text-lg">CS:GO</h6>
+            </div>
+          </div>
+          <div class="flex flex-row items-center gap-x-2">
+            <div class="hashtag-set">
+              <IconHash class="w-[10px] min-w-[10px] h-[10px] min-h-[10px]" />
+              <p class="font-secondary-bold text-sm">Ace</p>
+            </div>
+            <div class="hashtag-set">
+              <IconHash class="w-[10px] min-w-[10px] h-[10px] min-h-[10px]" />
+              <p class="font-secondary-bold text-sm">Ak47</p>
+            </div>
+            <div class="hashtag-set">
+              <IconHash class="w-[10px] min-w-[10px] h-[10px] min-h-[10px]" />
+              <p class="font-secondary-bold text-sm">Dust2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      id="comments"
+      class="w-full flex flex-col justify-center items-center flex-1 max-w-[360px] bg-[#555] rounded-2xl h-full"
+    >
+      <div id="no-comments" class="p-8 text-center">
+        <h5 class="font-secondary-bold text-xl text-white">No comments yet</h5>
+        <p class="font-secondary-bold text-white op-50">
+          Be the first one, who will do it :D
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped>
+#shortvideo {
+  width: 100%;
+  height: fit-content;
+}
+
+.maintag-set {
+  display: flex;
+  flex-direction: row;
+
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  color: rgba(255, 255, 255, 0.8);
+
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(2px);
+
+  padding: 4px 8px;
+  border-radius: 6px;
+
+  img {
+    width: 26px;
+    min-width: 26px;
+    height: 26px;
+    min-height: 26px;
+    border-radius: 99px;
+    padding: 2px;
+
+    border: 1px solid #21ea59;
+  }
+}
+
+.hashtag-set {
+  display: flex;
+  flex-direction: row;
+
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  color: rgba(255, 255, 255, 0.6);
+
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(2px);
+
+  padding: 4px 8px;
+  border-radius: 6px;
+}
+
+.action-btn {
+  width: 2.5rem;
+  height: 2.5rem;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 4px;
+  border-radius: 50%;
+  color: rgba(255, 255, 255, 0.8);
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(2px);
+}
+
+.active {
+  color: #21ea59;
+}
+
+.action-btn-mobile {
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 4px;
+  border-radius: 50%;
+  color: rgba(255, 255, 255, 0.8);
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(2px);
+}
+
+@media only screen and (max-width: 1024px) {
+  #shortvideo {
+  }
+}
+
+@media only screen and (orientation: portrait) and (max-width: 1280px) {
+  #inner-details {
+    display: none;
+  }
+
+  #outer-details {
+    display: flex;
+  }
+}
+
+@media only screen and (orientation: landscape) and (max-width: 1280px) {
+  #inner-details {
+    display: flex;
+  }
+
+  #outer-details {
+    display: none;
+  }
+}
+
+@media only screen and (max-width: 480px) and (min-height: 600px) {
+  #shortvideo {
+    /*  */
+  }
+
+  #inner-details {
+    display: none;
+  }
+
+  #outer-details {
+    padding: 1rem 1rem 2rem 1rem;
+    display: flex;
+  }
+}
+
+@media only screen and (min-width: 1280px) and (min-height: 600px) {
+  #inner-details {
+    display: none;
+  }
+
+  #outer-details {
+    padding: 0;
+    display: flex;
+  }
+}
+
+@media only screen and (min-width: 1280px) and (max-height: 600px) {
+  #comments {
+    display: none;
+  }
+}
+</style>
