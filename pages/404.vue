41009ddb4d5ad7b3d1ed35580e40507fe9d59391
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-10-27 20:33:24 +0600

    add: other pages

---
+++ pages/404.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>404</h1>
+  </div>
+</template>
+<style scoped></style>
---
+++ pages/settings.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>settings</h1>
+  </div>
+</template>
+<style scoped></style>
