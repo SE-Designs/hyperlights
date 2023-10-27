7dceeb4ccf7416b59dd9eeeddfddd71bd79d0c35
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-10-27 20:33:07 +0600

    add: index page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex flex-col gap-8 max-w-[1920px] mx-auto overflow-hidden">
+    <div class="flex flex-col gap-x-8 xl:flex-row">
+      <div id="recent" class="w-520px h-680px bg-[#555] rounded-3xl"></div>
+      <Shortvideo />
+    </div>
+  </div>
+</template>
+<style scoped>
+@media only screen and (max-width: 1720px) {
+  #recent {
+    display: none;
+  }
+}
+</style>
