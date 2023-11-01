7b9005e7b8efb2f9ffee01d0cc728dda5e157a18
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-01 21:42:56 +0600

    add: sign-social

---
+++ components/SignSocial.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex flex-col mt-12 max-w-[320px]">
+    <div
+      class="gap-x-2 rounded-lg text-center px-8 py-4 bg-[#eee] text-black cursor-pointer"
+    >
+      <h6 class="text-16px font-main-black">Join with Google</h6>
+    </div>
+  </div>
+</template>
+<style scoped></style>
