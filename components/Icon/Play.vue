6d5024aa691bd940246e9e756ffd654486d9cab4
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-02 03:01:54 +0600

    upd: sign-social

---
+++ components/Icon/Play.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 14 14"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M1.5 12.35a1.14 1.14 0 0 0 .63 1a1.24 1.24 0 0 0 1.22 0L12 8a1.11 1.11 0 0 0 0-2L3.35.69a1.24 1.24 0 0 0-1.22 0a1.14 1.14 0 0 0-.63 1Z"
+    ></path>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: "StreamlineEntertainmentControlButtonPlayButtonTelevisionButtonsMoviesPlayTvVideoControls",
+};
+</script>
--- components/SignSocial.vue
+++ components/SignSocial.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex flex-col mt-12 max-w-[320px]">
+  <div class="flex flex-col gap-y-2 mt-12 max-w-[320px]">
     <div
       class="gap-x-2 rounded-lg text-center px-8 py-4 bg-[#eee] text-black cursor-pointer"
     >
       <h6 class="text-16px font-main-black">Join with Google</h6>
+    </div>
+    <div
+      class="gap-x-2 rounded-lg text-center px-8 py-4 bg-[#eee] text-black cursor-pointer"
+    >
+      <h6 class="text-16px font-main-black">Join with Apple</h6>
     </div>
   </div>
 </template>
