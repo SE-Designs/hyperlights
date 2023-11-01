e132d43f2b654e140467a7cfa936f5537017bfbe
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-02 03:02:09 +0600

    upd: default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
+definePageMeta({
+  middleware: ["auth"],
+});
+
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
 const darkMode = ref(true);
+
+function showNotification(
+  head: string,
+  message: string,
+  type: "success" | "error"
+) {
+  toast.add({
+    severity: type,
+    summary: head,
+    detail: message,
+    life: 3000,
+  });
+}
 </script>
 
 <template>
   <LayoutNav />
-  <LayoutSidebar @update-theme="darkMode = !darkMode" />
+  <LayoutSidebar
+    @update-theme="darkMode = !darkMode"
+    @show-notification="showNotification"
+  />
   <main
     class="pt-0 pl-0 border-1 border-white w-screen min-h-screen xl:pt-[60px] sm:pl-[60px]"
   >
