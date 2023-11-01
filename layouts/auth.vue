e72515d25ae4f567e2799eb8f5542b004e2a9b31
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-02 03:00:43 +0600

    change: login -> auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="pt-0 pl-0 border-1 border-white w-screen min-h-screen">
+  <main
+    class="w-screen min-h-screen pt-0 pl-0 flex justify-center items-center"
+  >
     <slot />
   </main>
 </template>
