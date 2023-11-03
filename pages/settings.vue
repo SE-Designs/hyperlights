2dd4b4ee91f936583954c182b94f8fc25644dd87
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-03 18:26:33 +0600

    upd: settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,7 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const user = useSupabaseUser();
+const client = useSupabaseClient();
+const avatarFile = ref();
+
+// const { data, error } = await supabase
+//   .storage
+//   .from('avatars')
+//   .upload('public/avatar1.png', avatarFile, {
+//     cacheControl: '3600',
+//     upsert: false
+//   })
+
+const onUpload = (file: any) => {
+  console.log(file);
+};
+</script>
 <template>
-  <div>
-    <h1>settings</h1>
+  <div class="flex flex-col w-full">
+    <h1>Settings</h1>
+    <div class="flex flex-col gap-2 mt-4">
+      <label for="avatar">Avatar</label>
+      <FileUpload
+        ref="avatarFile"
+        id="avatar"
+        mode="basic"
+        name="avatar"
+        :file-limit="1"
+        :multiple="false"
+        accept="image/*"
+        @upload="onUpload"
+        :maxFileSize="5000000"
+        :auto="true"
+        chooseLabel="Browse"
+        aria-describedby="avatar-help"
+      >
+        <template #uploadicon>
+          <IconUpload class="mr-4" />
+        </template>
+      </FileUpload>
+      <small id="avatar-help">Max file size 5 MB</small>
+      {{ avatarFile }}
+    </div>
   </div>
 </template>
 <style scoped></style>
